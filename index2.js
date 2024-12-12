d1a0b2f4ca1884b0e6032795b8e5a77e8f167310
Natasciaruf <natasciarufino17@gmail.com>
2024-12-12 19:40:43 +0100

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,17 @@
+es_2.html:49 Live reload enabled.
+const paragrafi = document.querySelectorAll('.paragrafo')
+undefined
+paragrafi
+NodeList(3) [p.paragrafo, p.paragrafo, p.paragrafo]
+const elemento3 = document.getElementById('elemento-3')
+undefined
+elem
+VM379:1 Uncaught ReferenceError: elem is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM379:1
+elemento3
+<li id=​"elemento-3">​…​</li>​
+const stampaH2 = document.querySelectorAll('h2')
+undefined
+stampaH2
+NodeList(2) [h2, h2]
